7603cf46badf16eacad0b90e0403559d21f8c1e7
Makar <54309569+makfgrea@users.noreply.github.com>
2019-08-20 20:50:44 +0500

    v.1.0.0

---
+++ commands/cat.js
@@ -0,0 +1,18 @@
+const Discord = require("discord.js");
+const superagent = require("superagent");
+
+module.exports.run = async (bot, message, args) => {
+    let { body } = await superagent
+        .get(`http://aws.random.cat//meow`);
+
+    let dogembed = new Discord.RichEmbed()
+        .setColor("#ff9900")
+        .setTitle("Cat :cat:")
+        .setImage(body.file);
+
+    message.channel.send(dogembed);
+}
+
+module.exports.help = {
+    name: "cat"
+}
