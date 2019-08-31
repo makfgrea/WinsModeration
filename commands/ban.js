6820517197d95ee6193a9cac6a2f3addba338eb3
Makar <54309569+makfgrea@users.noreply.github.com>
2019-08-31 15:57:56 +0500

    v.2.0.0

--- commands/ban.js
+++ commands/ban.js
@@ -4,8 +4,8 @@ module.exports.run = async (bot, message, args) => {
     let bUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
     if (!bUser) return message.channel.send("Can't find user!");
     let bReason = args.join(" ").slice(22);
-    if (!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send("Error!")
-    if (bUser.hasPermission("ADMINISTRATOR")) return message.channel.send("That person can't be banned!");
+    if (!message.member.id === "348745835187666944") return message.reply("Sorry, you can't do that.");
+    if (message.member.id = "348745835187666944") return message.channel.send("That person can't be banned!");
 
     let banEmbed = new Discord.RichEmbed()
         .setDescription("~Ban~")
@@ -16,7 +16,7 @@ module.exports.run = async (bot, message, args) => {
         .addField("Time", message.createdAt)
         .addField("Reason", bReason);
 
-    let incidentchannel = message.guild.channels.find(`name`, "incidents");
+    let incidentchannel = message.guild.channels.find(`name`, "bot-spam");
     if (!incidentchannel) return message.channel.send("Can't find incidents channel.");
 
     message.guild.member(bUser).ban(bReason);
